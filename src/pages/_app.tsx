fcea1e66f4d3f0f3f7bf7c7edeb9afdc65e65afe
Abdullah Faruk <afgonullu@gmail.com>
2023-03-01 09:36:40 +0300

    refactor: fix linting errors of boilerplate code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import type { AppProps } from 'next/app';
+
+const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />;
+
+export default App;
